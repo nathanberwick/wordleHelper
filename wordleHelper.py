38f23757fe45302367c4cd27698e22b72660d30d
Nathan Berwick <nathan.berwick@iis.fraunhofer.de>
2022-01-26 16:43:09 +0100

    add size error checking for green letters

--- wordleHelper.py
+++ wordleHelper.py
@@ -3,15 +3,44 @@ def load_words():
         valid_words = set(word_file.read().split())
 
     return valid_words
+    
+word_length = 5
 
 if __name__ == '__main__':
     english_words = load_words()
     
     #extract five letter words
-    print(len(english_words))
     five_letter_words = []
     for x in english_words:
-        if(len(x) == 5):
+        if(len(x) == word_length):
             five_letter_words.append(x)
-    print(five_letter_words)
+    #print(five_letter_words)
+    
+print ("enter green letters. Press enter to skip")
+green = []
+for counter in range(word_length):
+    string = 'enter green letter ' + str(counter) + ': '
+    newGreen = input(string)
+    if len(newGreen) > 1:
+        print ("too big! extracting first letter")
+        newGreen = newGreen[0]
+        print (newGreen)
+    green.append(newGreen)
+    
+print ("enter yellow letters. Press enter when finished")
+yellow = []
+yellowCheck = True
+while yellowCheck:
+    string = 'enter yellow letter: '
+    newYellow = input(string)
+    print (newYellow)
+    #if(newYellow == ''):
+    #    print ("exiting yellows")
+    #    yellowCheck = False
+    yellow.append(input(string))
+    
+    
+
+#for counter in range(word_length):
+#    print(counter)
 
