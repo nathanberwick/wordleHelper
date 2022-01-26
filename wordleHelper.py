bd30857cb0319ac30235d90992bf7bd850607bfb
Nathan Berwick <nathan.berwick@iis.fraunhofer.de>
2022-01-26 16:16:51 +0100

    extracted five letter words

--- wordleHelper.py
+++ wordleHelper.py
@@ -4,10 +4,14 @@ def load_words():
 
     return valid_words
 
-
 if __name__ == '__main__':
     english_words = load_words()
-    # demo print
-    print('fate' in english_words)
     
-    print(english_words)
+    #extract five letter words
+    print(len(english_words))
+    five_letter_words = []
+    for x in english_words:
+        if(len(x) == 5):
+            five_letter_words.append(x)
+    print(five_letter_words)
+
